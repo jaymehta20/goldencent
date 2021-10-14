f41f0850ac7b46e7aece1674e17514b8f2b9c4fd
Mash Codee <codeemash@gmail.com>
2021-10-14 13:00:03 +0530

    journey section added

--- build/js/app.js
+++ build/js/app.js
@@ -1,14 +1,20 @@
 const topBtn = document.querySelector('.topBtn')
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > window.innerHeight * 3 / 4 && !topBtn.classList.contains('visible')) {
+    if (
+        window.scrollY > window.innerHeight * 3 / 4
+        &&
+        !topBtn.classList.contains('visible')
+    ) {
         topBtn.classList.add('visible')
     }
-    else if (window.scrollY < window.innerHeight * 3 / 4 && topBtn.classList.contains('visible')) {
+    else if (
+        window.scrollY < window.innerHeight * 3 / 4
+        &&
+        topBtn.classList.contains('visible')
+    ) {
         topBtn.classList.remove('visible')
     }
 })
 
-topBtn.addEventListener('click', () => {
-    window.scroll(0, 0)
-})
+topBtn.addEventListener('click', () => window.scroll(0, 0))
