47da017775b87a53f596ca8df88ca7042d4aa4b2
Mash Codee <codeemash@gmail.com>
2021-10-16 14:56:45 +0530

    major changes

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,19 @@
 const topBtn = document.querySelector('.topBtn')
-const contactBtn = document.getElementById('navigation__item--contact')
-const aboutBtn = document.getElementById('navigation__item--about')
-const journeyBtn = document.getElementById('navigation__item--journey')
-const productsNavBtn = document.getElementById('navigation__item--products')
-const productsBtn = document.getElementById('productsBtn')
+const hamburger = document.querySelector('.hamburger')
+const navmob = document.querySelector('#navmob')
+
+const contactBtn = document.querySelector('.navigation__item--contact')
+const aboutBtn = document.querySelector('.navigation__item--about')
+const journeyBtn = document.querySelector('.navigation__item--journey')
+const productsBtn = document.querySelector('.navigation__item--products')
+
+const mobContactBtn = document.querySelector('.navigation__mob__item--contact')
+const mobAboutBtn = document.querySelector('.navigation__mob__item--about')
+const mobJourneyBtn = document.querySelector('.navigation__mob__item--journey')
+const mobProductsBtn = document.querySelector('.navigation__mob__item--products')
+
+const heroProductsBtn = document.querySelector('.productsBtn')
+
 const heroSection = document.getElementById('hero')
 const contactSection = document.getElementById('contact')
 const aboutSection = document.getElementById('about')
@@ -30,8 +40,35 @@ window.addEventListener('scroll', () => {
 
 // navigation btn smooth scroll
 topBtn.addEventListener('click', () => window.scroll(0, 0))
+
 contactBtn.addEventListener('click', () => window.scroll(0, contactSection.offsetTop))
 journeyBtn.addEventListener('click', () => window.scroll(0, journeySection.offsetTop))
-productsNavBtn.addEventListener('click', () => window.scroll(0, productsSection.offsetTop))
-productsBtn.addEventListener('click', () => window.scroll(0, productsSection.offsetTop))
 aboutBtn.addEventListener('click', () => window.scroll(0, aboutSection.offsetTop))
+productsBtn.addEventListener('click', () => window.scroll(0, productsSection.offsetTop))
+
+mobContactBtn.addEventListener('click', () => {
+    hamburger.classList.remove('visible')
+    navmob.classList.remove('visible')
+    window.scroll(0, contactSection.offsetTop)
+})
+mobJourneyBtn.addEventListener('click', () => {
+    hamburger.classList.remove('visible')
+    navmob.classList.remove('visible')
+    window.scroll(0, journeySection.offsetTop)
+})
+mobAboutBtn.addEventListener('click', () => {
+    hamburger.classList.remove('visible')
+    navmob.classList.remove('visible')
+    window.scroll(0, aboutSection.offsetTop)
+})
+mobProductsBtn.addEventListener('click', () => {
+    hamburger.classList.remove('visible')
+    navmob.classList.remove('visible')
+    window.scroll(0, productsSection.offsetTop)
+})
+
+heroProductsBtn.addEventListener('click', () => window.scroll(0, productsSection.offsetTop))
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('visible')
+    navmob.classList.toggle('visible')
+})
